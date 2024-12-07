ebbe376970852929226fabb724a1074976a7e6c7
nadermgr@gmail.com <nadermgr@gmail.com>
2024-12-06 20:26:28 -0500

    super_admin2

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -10,17 +10,13 @@ use Spatie\Permission\Models\Role;
 class SuperAdminSeeder extends Seeder
 {
     public function run()
-{
-    $user = User::firstOrCreate([
-        'email' => env('SUPER_ADMIN_EMAIL', 'soy@nader.com'),
-    ], [
-        'name' => env('SUPER_ADMIN_NAME', 'Nader'),
-        'password' => bcrypt(env('SUPER_ADMIN_PASSWORD', 'password')),
-    ]);
-
-    $role = Role::firstOrCreate(['name' => 'super_admin']);
-    $user->assignRole($role);
-}
+    {
+        User::create([
+            'name' => 'Nader',
+            'email' => 'soy@nader.com',
+            'password' => Hash::make('tuContraseñaSegura')
+        ]);
+    }
 }
 
 
